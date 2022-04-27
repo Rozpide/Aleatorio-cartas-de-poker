7da15a8d0d8f89316475d23dd20a6845df56f356
Rozpi <joserozpide@yahoo.es>
2022-04-27 15:02:13 +0000

    Nuevorepo-cartas-aleatorias

---
+++ cartas.js
@@ -0,0 +1,37 @@
+window.onload = function() {
+    
+    document.querySelector("#boton").addEventListener("click", function() {
+      let valor = [
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10",
+        "J",
+        "Q",
+        "K"
+      ];
+      document.querySelector(".Reemplazar").innerHTML =
+        valor[RandomCards(0, valor.length)];
+      console.log(valor[RandomCards(0, valor.length - 1)]);
+      Probando();
+      console.log(card[RandomCards(0, card.length - 1)]);
+    });
+    let card = ["Pica", "Trebol", "Corazones", "Diamante"];
+  
+    function RandomCards(min, max) {
+      return Math.floor(Math.random() * (max - min) + min);
+    }
+    function Probando() {
+      document.querySelector(".valor").className =
+        "valor " + card[RandomCards(0, card.length)];
+    }
+
+  };
+  
+ 
